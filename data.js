14cbd078ea43443ed3d71aa255371a6bb7882884
RenanKabbach <renanbotarelli@gmail.com>
2020-12-31 02:12:25 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free",
+    featured: true,
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desenvolvendo o CSS Grid na prática | Mayk Brito",
+    duration: "36 min",
+    price: "Free",
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Array: Higher Order Functions | Mayk Brito",
+    duration: "54 min",
+    price: "Free",
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "Formulário animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "33 minn",
+    price: "Free",
+    featured: true,
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a variável this no Javascript | Mayk Brito",
+    duration: "48 min",
+    price: "Free",
+  },
+  {
+    id: "2alg7MQ6_sI",
+    title:
+      "Como usar Git e Github na prática: Guia para inciantes | Mayk Brito",
+    duration: "33 min",
+    price: "Free",
+    featured: true,
+  },
+];
